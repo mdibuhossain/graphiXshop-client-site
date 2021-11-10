600d9fcbe675e64231b1b6969b2125c0e8ffb205
Ibrahim H <mohammad.ihakash@gmail.com>
2021-11-10 23:43:55 +0600

    banner added

---
+++ src/Components/Home/BannerSlider.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import banner1 from '../../images/b1.jpeg';
+import banner2 from '../../images/b2.jpeg';
+import banner3 from '../../images/b3.jpeg';
+import banner4 from '../../images/b4.jpeg';
+
+const slideList = [banner1, banner2, banner3, banner4];
+
+const BannerSlider = () => {
+    return (
+        <Slider autoplay={3000}>
+            {slideList.map((item, index) => (
+                <div
+                    key={index}
+                    style={{ background: `url('${item}') no-repeat center center`, backgroundSize: '100% 100%', height: '100%' }}
+                />
+            ))}
+        </Slider>
+    );
+};
+
+export default BannerSlider;
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BannerSlider from '../Components/Home/BannerSlider';
+import Navigation from '../Components/Shared/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation />
+            <BannerSlider />
+        </div>
+    );
+};
+
+export default Home;
